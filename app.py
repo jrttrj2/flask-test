a739c3f38a2b2171c4d3912470e17fe00dcb623a
jrttrj2 <jrttrj2@gmail.com>
2022-03-10 19:26:22 -0500

    Added config.py, updated app to read from config

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import sys
 from flask import Flask
 
 app = Flask("Flask Test")
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "Hello World, it is I!"
+    secret_key = app.config.get("SECRET_KEY")
+    return "Hello World! The secret key is " + secret_key + "."
